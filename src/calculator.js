dd21df03b5f7145d8460eaf9e69f8849f760b679
DeveloperNinja12 <p.gorav2011@gmail.com>
2025-10-08 17:15:24 +0530

    initialized setup

---
+++ src/calculator.js
@@ -0,0 +1,10 @@
+/*  */
+class Calculator {
+    static add (testString){
+        return parseInt(testString.split(",")[0]) + parseInt(testString.split(",")[1]);
+    }
+};
+
+(function main(){
+    
+})();
